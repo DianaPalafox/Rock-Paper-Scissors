cc549268216abe9af8aea6866dfb8b066340cce2
Diana Palafox <dianapalafox95@gmail.com>
2022-03-19 13:00:15 -0700

    Add the score to the game

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
 
 
+let number=0; 
+let score= 0; 
+
 function computerPlay() {
 let play = ['rock', 'paper', 'scissors'];
-let result= play[Math.floor(Math.random()*play.length)];
-console.log(result); 
-return result;
+return play[Math.floor(Math.random()*play.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection=== 'paper') {
-        return 'You lose! Paper beats Rock'; 
+        alert('You lose! Paper beats Rock'); 
+         
     }
     else if (playerSelection === 'paper' && computerSelection=== 'rock') {
-        return 'You win! Paper beats Rock'; 
+        alert('You win! Paper beats Rock'); 
+        addUserScore();
     }
     else if (playerSelection === 'scissors' && computerSelection=== 'paper') {
-        return 'You win! Scissors beats Paper'; 
+        alert('You win! Scissors beats Paper'); 
+        addUserScore();
     }
     else if (playerSelection === 'paper' && computerSelection=== 'scissors') {
-        return 'You lose! Scissors beats Paper';
+        alert('You lose! Scissors beats Paper');
+        
     }
     else if (playerSelection === 'scissors' && computerSelection=== 'rock') {
-        return 'You lose! Rock beats Scissors';
+        alert('You lose! Rock beats Scissors');
+        
     }
     else if (playerSelection === 'rock' && computerSelection=== 'scissors') {
-        return 'You win! Rock beats Scissors';
+        alert('You win! Rock beats Scissors');
+        addUserScore();
     }
-    else  {
-        return 'Both players chose the same, it\'s a tie'; 
+    else if (playerSelection === 'rock' && computerSelection=== 'rock') {
+        alert('It\'s a tie!'); 
+        
     }
-
-   
+    else if (playerSelection === 'paper' && computerSelection=== 'paper') {
+        alert('It\'s a tie!'); 
+        
+    }
+    else if (playerSelection === 'scissors' && computerSelection=== 'scissors') {
+        alert('It\'s a tie!'); 
+        
+    }
+    else {
+        alert('Please chose between rock, paper and scissors'); 
+    }
   }
+  function numberRound() {
+    alert("Game " + number); 
+  }
+
+  function addUserScore() {
+      score++; 
+    console.log(score); 
+  }
+
+   function finalScore() {
+       if (score >= 3) {
+           alert('Congratuletions! You win');
+       }
+       else if (score < 3){
+           alert('Sorry, you lost'); 
+       }
+   }
   
- 
-  const playerSelection ='rock';
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+        number++; 
+        numberRound();
+        let playerSelection = prompt("Please chose between rock, paper and scissors: ")
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);    
+     }
+  }
+
+game();
+finalScore(); 
+  
+
+  
